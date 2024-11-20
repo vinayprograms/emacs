5411913568a16ea0308d1f928b00bbc5b290c43c
Vinay Venkatesh <>
2024-11-20 11:46:30 -0500

    fixes

--- evil.el
+++ evil.el
@@ -38,25 +38,17 @@
     "Update mode line color based on Evil state."
     (progn
       (cond
-       ((evil-insert-state-p) (set-face-background 'mode-line "red"))
-       ((evil-visual-state-p) (set-face-background 'mode-line "blue"))
-       ((evil-replace-state-p) (set-face-background 'mode-line "green"))
-       (t (set-face-background 'mode-line "gray20")))
-      (cond
-       ((evil-insert-state-p) (send-string-to-terminal "\e[5 q"))
-       ((evil-visual-state-p) (send-string-to-terminal "\e[6 q"))
-       ((evil-replace-state-p) (send-string-to-terminal "\e[4 q"))
-       (t (send-string-to-terminal "\e[1 q")))))  ;; Default color for normal mode
-  (defun my-set-default-cursor-before-exit ()
-    "Set the cursor to the default block shape before Emacs exits."
-    (send-string-to-terminal "\e[2 q")) ;; Block cursor (normal mode)
-  (add-hook 'kill-emacs-hook #'my-set-default-cursor-before-exit)
-
+       ((evil-insert-state-p) (set-face-background 'mode-line "red" (selected-frame) ))
+       ((evil-visual-state-p) (set-face-background 'mode-line "blue" (selected-frame) ))
+       ((evil-replace-state-p) (set-face-background 'mode-line "green" (selected-frame) ))
+       (t (set-face-background 'mode-line "gray20" (selected-frame))))
+      ;; (cond
+      ;;  ((evil-insert-state-p) (send-string-to-terminal "\e[5 q"))
+      ;;  ((evil-visual-state-p) (send-string-to-terminal "\e[6 q"))
+      ;;  ((evil-replace-state-p) (send-string-to-terminal "\e[4 q"))
+      ;;  (t (send-string-to-terminal "\e[1 q")))
+       ))  ;; Default color for normal mode
   
-  ;; Configure cursor shape for different Evil modes
-  (setq evil-normal-state-cursor 'box)        ;; Block cursor for normal mode
-  (setq evil-insert-state-cursor 'bar)        ;; Bar cursor for insert mode
-  (setq evil-replace-state-cursor 'hbar)      ;; Underscore cursor for replace mode
   ;; Add hooks to change the mode line color when entering each state
   (add-hook 'evil-insert-state-entry-hook #'my-update-mode-line-color)
   (add-hook 'evil-visual-state-entry-hook #'my-update-mode-line-color)
@@ -65,7 +57,8 @@
   (add-hook 'evil-replace-state-entry-hook #'my-update-mode-line-color)
   (add-hook 'evil-emacs-state-entry-hook #'my-update-mode-line-color)
 
-  ;; Initialize mode line color when Emacs starts
-  (add-hook 'after-init-hook #'my-update-mode-line-color))
+  ;; Initialize mode line color when emacs starts
+  (add-hook 'after-init-hook #'my-update-mode-line-color)
+)
 
 ;;; evil.el ends here
