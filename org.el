bf451005c77f92159fc0b16efd34bd72e01a2e08
Vinay Venkatesh <>
2025-05-05 16:57:57 -0400

    Fix

--- org.el
+++ org.el
@@ -117,7 +117,7 @@
 	    ((org-agenda-overriding-header "=*= PROJECTS =*=")))
       (todo "WAITING"
 	    ((org-agenda-overriding-header "=*= WAITING FOR =*=")))
-      (todo "SPRINT-TASK"
+      (todo "ITEM"
 	    ((org-agenda-overriding-header "=*= SPRINT TASKS =*=")))
       (alltodo ""
 	       ((org-agenda-overriding-header "=*= ALL =*=")
@@ -221,5 +221,3 @@
         org-superstar-special-todo-items t))
 
 ;;; org.el ends here
-
-
